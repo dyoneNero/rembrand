d9bed4343aafe9bb39184e905c0f3bb28622197f
dyoneNero <dyonenero@gmail.com>
2023-04-10 12:26:20 +0300

    react-router-3

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 import './index.scss';
-
-import Main from "./pages/main/main";
-import AdminLogin from "./pages/admin/admin-login";
-import AdminSettings from "./pages/admin/admin-settings";
-import Works from "./pages/works/works";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <HashRouter>
-          <Routes>
-              <Route path="/" element={<Main/>}/>
-              <Route path="/works/:id" element={<Works/>}/>
-              <Route path="/admin" element={<AdminLogin/>}/>
-              <Route path='/admin/settings/*' element={<AdminSettings/>}/>
-              <Route path='*' element={<Navigate to ="/"/>}/>
-          </Routes>
-      </HashRouter>
+      <BrowserRouter>
+          <App/>
+      </BrowserRouter>
   </React.StrictMode>
 );
