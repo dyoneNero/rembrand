d100e36546edd80bc2f9a11db3c421927aa2edc5
dyoneNero <dyonenero@gmail.com>
2023-04-11 09:24:41 +0300

    with-main-block

--- client/src/pages/main/main.js
+++ client/src/pages/main/main.js
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import BackGround from "../../components/main/background/BackGround";
-// import MainSection from "../../components/main/mainSection/MainSection";
 
 import style from './main.module.scss'
+
+import MainSection from "../../components/main/mainSection/MainSection";
 // import AboutUs from "../../components/main/aboutUsSection/AboutUs";
 // import SectionWithSwiper from "../../components/main/SectionWithSwiper/SectionWithSwiper";
-//
 // import stocksSettings from './sectionWithSwiper/stocks/settings.json'
 // import worksSettings from './sectionWithSwiper/works/settings.json'
-//
-//
 // import ServicesSection from "../../components/main/servicesSection/ServicesSection";
 // import ContactLocationSection from "../../components/main/contactLocationSection/ContactLocationSection";
+
 import Footer from "../../components/main/footer/Footer";
 import Header from "../../components/main/header/Header";
 import {getInfo} from "./service";
@@ -30,7 +29,7 @@ const MainPage = () => {
             <Header headerTel={info.headerTel} shadow={true}/>
             <BackGround/>
             <div className={style.container}>
-                {/*<MainSection mainTitle={info.mainTitle} mainSubtitle={info.mainSubtitle}/>*/}
+                <MainSection mainTitle={info.mainTitle} mainSubtitle={info.mainSubtitle}/>
                 {/*<AboutUs aboutUsSlogan={info.aboutUsSlogan}/>*/}
                 {/*<ServicesSection/>*/}
                 {/*<SectionWithSwiper path='stocks' setting={stocksSettings.swiperSettings} name={stocksSettings.name} type={stocksSettings.type} anchor={stocksSettings.anchor}/>*/}
