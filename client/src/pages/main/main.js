8b93536d9d41c7915e2569039dc999d02fb94e71
dyoneNero <dyonenero@gmail.com>
2023-04-11 09:49:26 +0300

    mm2

--- client/src/pages/main/main.js
+++ client/src/pages/main/main.js
@@ -7,8 +7,8 @@ import MainSection from "../../components/main/mainSection/MainSection";
 import AboutUs from "../../components/main/aboutUsSection/AboutUs";
 // import ServicesSection from "../../components/main/servicesSection/ServicesSection";
 import SectionWithSwiper from "../../components/main/SectionWithSwiper/SectionWithSwiper";
-import stocksSettings from './sectionWithSwiper/stocks/settings.json'
-// import worksSettings from './sectionWithSwiper/works/settings.json'
+//import stocksSettings from './sectionWithSwiper/stocks/settings.json'
+import worksSettings from './sectionWithSwiper/works/settings.json'
 // import ContactLocationSection from "../../components/main/contactLocationSection/ContactLocationSection";
 
 import Footer from "../../components/main/footer/Footer";
@@ -33,8 +33,8 @@ const MainPage = () => {
                 <MainSection mainTitle={info.mainTitle} mainSubtitle={info.mainSubtitle}/>
                 <AboutUs aboutUsSlogan={info.aboutUsSlogan}/>
                 {/*<ServicesSection/>/!*problem n.map*!/*/}
-                <SectionWithSwiper path='stocks' setting={stocksSettings.swiperSettings} name={stocksSettings.name} type={stocksSettings.type} anchor={stocksSettings.anchor}/>
-                {/*<SectionWithSwiper path='services' setting={worksSettings.swiperSettings} name={worksSettings.name} type={worksSettings.type} anchor={worksSettings.anchor}/>*/}
+                {/*<SectionWithSwiper path='stocks' setting={stocksSettings.swiperSettings} name={stocksSettings.name} type={stocksSettings.type} anchor={stocksSettings.anchor}/> 301*/}
+                <SectionWithSwiper path='services' setting={worksSettings.swiperSettings} name={worksSettings.name} type={worksSettings.type} anchor={worksSettings.anchor}/>
                 {/*<ContactLocationSection/>*/}
             </div>
             <Footer copyright={info.copyright}/>
