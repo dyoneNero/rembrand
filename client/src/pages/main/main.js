3bf0559f74f20c3c6e707c4dd9887ad10a05d19c
dyoneNero <dyonenero@gmail.com>
2023-04-11 09:46:46 +0300

    mm2

--- client/src/pages/main/main.js
+++ client/src/pages/main/main.js
@@ -5,9 +5,9 @@ import style from './main.module.scss'
 
 import MainSection from "../../components/main/mainSection/MainSection";
 import AboutUs from "../../components/main/aboutUsSection/AboutUs";
-import ServicesSection from "../../components/main/servicesSection/ServicesSection";
-// import SectionWithSwiper from "../../components/main/SectionWithSwiper/SectionWithSwiper";
-// import stocksSettings from './sectionWithSwiper/stocks/settings.json'
+// import ServicesSection from "../../components/main/servicesSection/ServicesSection";
+import SectionWithSwiper from "../../components/main/SectionWithSwiper/SectionWithSwiper";
+import stocksSettings from './sectionWithSwiper/stocks/settings.json'
 // import worksSettings from './sectionWithSwiper/works/settings.json'
 // import ContactLocationSection from "../../components/main/contactLocationSection/ContactLocationSection";
 
@@ -32,8 +32,8 @@ const MainPage = () => {
             <div className={style.container}>
                 <MainSection mainTitle={info.mainTitle} mainSubtitle={info.mainSubtitle}/>
                 <AboutUs aboutUsSlogan={info.aboutUsSlogan}/>
-                <ServicesSection/>
-                {/*<SectionWithSwiper path='stocks' setting={stocksSettings.swiperSettings} name={stocksSettings.name} type={stocksSettings.type} anchor={stocksSettings.anchor}/>*/}
+                {/*<ServicesSection/>/!*problem n.map*!/*/}
+                <SectionWithSwiper path='stocks' setting={stocksSettings.swiperSettings} name={stocksSettings.name} type={stocksSettings.type} anchor={stocksSettings.anchor}/>
                 {/*<SectionWithSwiper path='services' setting={worksSettings.swiperSettings} name={worksSettings.name} type={worksSettings.type} anchor={worksSettings.anchor}/>*/}
                 {/*<ContactLocationSection/>*/}
             </div>
