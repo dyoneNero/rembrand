60eddfa250c4afd104b419088e31ef0f5f4bc72a
dyoneNero <dyonenero@gmail.com>
2023-04-11 04:26:02 +0300

    react-router-app3

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,36 @@ import Main from "./pages/main/main";
 import Works from "./pages/works/works";
 import AdminLogin from "./pages/admin/admin-login";
 import AdminSettings from "./pages/admin/admin-settings";
-import {Navigate, Route, Routes} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main/>
+    },
+    {
+        path: "/admin",
+        element: <AdminLogin/>
+    },
+    {
+        path: "/works/:id",
+        element: <Works/>
+    },
+    {
+        path: "/admin/settings/*",
+        element: <AdminSettings/>
+    }
+])
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Main/>}/>
-            <Route path="/app/works/:id" element={<Works/>}/>
-            <Route path="/admin" element={<AdminLogin/>}/>
-            <Route path='/admin/settings/*' element={<AdminSettings/>}/>
-        </Routes>
-    );
+    return <RouterProvider router={router}/>
 };
 
 export default App;
+
+// <Routes>
+//     <Route path="/" element={<Main/>}/>
+//     <Route path="/works/:id" element={<Works/>}/>
+//     <Route path="/admin" element={<AdminLogin/>}/>
+//     <Route path='/admin/settings/*' element={<AdminSettings/>}/>
+// </Routes>
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {BrowserRouter} from "react-router-dom";
-
 import './index.scss';
 import App from "./App";
 
@@ -10,8 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App/>
-      </BrowserRouter>
+      <App/>
   </React.StrictMode>
 );
