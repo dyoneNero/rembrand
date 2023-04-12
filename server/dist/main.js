151ece9b0a9ed720372eecefd746c7553d6e0745
dyoneNero <dyonenero@gmail.com>
2023-04-12 05:35:29 +0300

    mm2

--- server/dist/main.js
+++ server/dist/main.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const process = require("process");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix("api");
     app.enableCors();
-    await app.listen(4200);
+    await app.listen(process.env.PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix("api")
   app.enableCors()
-  await app.listen(4200);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
