2589f2c3914bb67a5c7f972dd1869556b1eb5cd3
dyoneNero <dyonenero@gmail.com>
2023-04-14 12:55:26 +0300

    demo-ura

--- client/src/pages/main/main.js
+++ client/src/pages/main/main.js
@@ -6,10 +6,10 @@ import style from './main.module.scss'
 import MainSection from "../../components/main/mainSection/MainSection";
 import AboutUs from "../../components/main/aboutUsSection/AboutUs";
 import ServicesSection from "../../components/main/servicesSection/ServicesSection";
-// import SectionWithSwiper from "../../components/main/SectionWithSwiper/SectionWithSwiper";
-//import stocksSettings from './sectionWithSwiper/stocks/settings.json'
-// import worksSettings from './sectionWithSwiper/works/settings.json'
-// import ContactLocationSection from "../../components/main/contactLocationSection/ContactLocationSection";
+import SectionWithSwiper from "../../components/main/SectionWithSwiper/SectionWithSwiper";
+import stocksSettings from './sectionWithSwiper/stocks/settings.json'
+import worksSettings from './sectionWithSwiper/works/settings.json'
+import ContactLocationSection from "../../components/main/contactLocationSection/ContactLocationSection";
 
 import Footer from "../../components/main/footer/Footer";
 import Header from "../../components/main/header/Header";
@@ -33,10 +33,9 @@ const MainPage = () => {
                 <MainSection mainTitle={info.mainTitle} mainSubtitle={info.mainSubtitle}/>
                 <AboutUs aboutUsSlogan={info.aboutUsSlogan}/>
                 <ServicesSection/>
-                {/*problem n.map*/}
-                {/*<SectionWithSwiper path='stocks' setting={stocksSettings.swiperSettings} name={stocksSettings.name} type={stocksSettings.type} anchor={stocksSettings.anchor}/> 301*/}
-                {/*<SectionWithSwiper path='services' setting={worksSettings.swiperSettings} name={worksSettings.name} type={worksSettings.type} anchor={worksSettings.anchor}/> 301*/}
-                {/*<ContactLocationSection/> length*/}
+                <SectionWithSwiper path='stocks' setting={stocksSettings.swiperSettings} name={stocksSettings.name} type={stocksSettings.type} anchor={stocksSettings.anchor}/>
+                <SectionWithSwiper path='services' setting={worksSettings.swiperSettings} name={worksSettings.name} type={worksSettings.type} anchor={worksSettings.anchor}/>
+                <ContactLocationSection/>
             </div>
             <Footer copyright={info.copyright}/>
         </div>
