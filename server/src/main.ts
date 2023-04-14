d98d5cb8d1c535ca1e71294dea40767aa911d720
dyoneNero <dyonenero@gmail.com>
2023-04-14 17:33:22 +0300

    demo

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix("api")
-  app.enableCors({
-    credentials: true,
-    origin: true
-  })
+  app.enableCors()
   await app.listen(process.env.PORT);
 }
 bootstrap();
